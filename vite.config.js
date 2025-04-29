bdd6c8c07123b8eb4f22d7b8d04ea10487b3fff2
Gonzalo Calderon <gonzacalderon13788@mail.com>
2025-04-29 20:55:27 -0300

    chore: add Netlify SPA config via netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { resolve } from 'path';
+import fs from 'fs';
+
 
 export default defineConfig({
   plugins: [
