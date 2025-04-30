3ce9867a4a98383b8c41c5425a98ced14df2121b
Gonzalo Calderon <gonzacalderon13788@mail.com>
2025-04-29 22:05:20 -0300

    correcion wa

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -4,20 +4,20 @@ import { Fab } from '@mui/material';
 const WhatsAppButton = () => {
   return (
     <Fab
-    color="success"
-    href="https://wa.me/5492614601788"
-    target="_blank"
-    rel="noopener noreferrer"
-    sx={{
-      position: 'fixed',
-      bottom: 16,
-      right: 16,
-      zIndex: 2000
-    }}
-  >
-    <WhatsAppIcon />
-  </Fab>
-  
+      color="success"
+      component="a"
+      href="https://wa.me/5492614601788"
+      target="_blank"
+      rel="noopener noreferrer"
+      sx={{
+        position: 'fixed',
+        bottom: 16,
+        right: 16,
+        zIndex: 2000
+      }}
+    >
+      <WhatsAppIcon />
+    </Fab>
   );
 };
 
